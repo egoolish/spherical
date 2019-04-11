f297cb435d558baa1ff36095a1bd5f29fc88fd70
egoolish <32001740+egoolish@users.noreply.github.com>
2019-04-11 18:33:07 -0400

    Fixed Legendre expansion

--- approximation.m
+++ approximation.m
@@ -1,25 +1,71 @@
-theta = linspace(-pi/2, pi/2, 720000);
-z1 = linspace(0, 1, 360000);
-z2 = linspace(1, 0, 360000);
-z = [z1, z2];
-max = 20;
+global a
+global b
+global c
+global d
+global e
+global f
+global g
+global h
+global mySphFun
+syms p t
 
-%fun = @(p, t) sin(t).^3.*cos(p).*sin(p).^2 + 2*sin(t).^2.*cos(p).*sin(p).*cos(t) - sin(t).^3.*cos(p).^3;
+a = -1;
+b = 3;
+c = -6;
+d = 3;
+e = 1;
+f = -1;
+g = 2;
+h = -1;
 
-Plm = legendre(0,cos(theta), 'sch');
-sum = Plm;
-for t = 1:max
-    disp(t);
-    l = t;
-    Plm = legendre(l,cos(theta), 'sch');
-    sum = sum + Plm(1, :); 
-    clear Plm
+% myCartFun = @(x, y, z) x.^2 + y.*z;
+% mySphFun = @(p, t) (cos(p).*sin(t)).^2 + (sin(t).*sin(p).*cos(t));
+
+% myCartFun = @(x, y, z) a.*x.*x + b.*x.*x.*x + c.* x.* y + d.*y.*z + e.*x.*z + f.*x.*y.*z ...
+%     + g.*z.*z.*z.*z + h.*z.*y.*x.*x;
+ 
+mySphFun = a.*(sin(t).*cos(p)).^2 + b.*(sin(t).*cos(p)).^3 + c.*(sin(t).*cos(p)).*(sin(t).*sin(p))...
+    + d.*(sin(t).*sin(p)).*(cos(t)) + e.*(sin(t).*cos(p)).*(cos(t)) + ...
+    f.*(sin(t).*cos(p)).*(sin(t).*sin(p)).*(cos(t)) + g.*(cos(t)).^4 + h.*(cos(t)).*(sin(t).*sin(p)).*(sin(t).*cos(p)).^2;
+
+syms t p 
+approx = myApprox(mySphFun, 50);
+t1 = 0.5;
+p1 = 0.7;
+disp(double(subs(approx, [t, p], [t1, p1])));
+disp(double(subs(mySphFun, [t, p], [t1, p1])));
+
+
+function output = myApprox(fun, max)
+    syms p t
+    output = 0;
+    for l = 0:max
+        disp(l);
+        fun_real = sin(t).*fun.*getLegendre(l, cos(t));
+        coeff = ((2.*l+1)./2) .* int(fun_real, t, 0, pi);
+        output = output + coeff.*getLegendre(l, cos(t));
+    end
 end
 
-f1 = figure;
-plot(theta, sum);
-title(['Sum Zonal Harmonics L = 0:', num2str(max)]);
+function out = getLegendre(l, t)
+    whole = legendreP(l, t);
+    out = whole(1, :);
+end
 
-f2 = figure;
-plot(theta, sum/(max+1));
-title(['Average Zonal Harmonics L = 0:', num2str(max)]);
+% function output = myApprox(x, y, z, max)
+%     global mySphFun
+%     r = sqrt(x.^2+y.^2+z.^2);
+%     t = acos(z/r);
+%     output = 0;
+%     for l = 0:max
+%         disp("l : " + num2str(l) + ", x : " + num2str(output));
+%         fun_l = @(p, t) sin(t) .* mySphFun(p, t) .* getLegendre(t, l);
+%         coeff_l = integral2(fun_l, 0, 2*pi, 0, pi, 'AbsTol', 1e-12);
+%         output = output + (coeff_l .* getLegendre(t, l));
+%     end
+% end
+% 
+% function out = getLegendre(t, l)
+%     whole = legendre(l, cos(t), 'sch');
+%     out = squeeze(whole(1, :, :));
+% end
